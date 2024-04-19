ad7fbf1406af3effa6a6fa56789549c7fb44d84a
Sonalp98 <165366403+Sonalp98@users.noreply.github.com>
2024-04-19 13:22:18 +0530

    Add files via upload

---
+++ 19(Salary greater than avg salary).sql
@@ -0,0 +1,36 @@
+-- Day 19/50 SQL Challenge
+
+DROP TABLE IF EXISTS employees;
+-- Creating the employees table
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(100),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+
+-- Inserting sample data for employees
+INSERT INTO employees (employee_name, department, salary) 
+VALUES
+    ('John Doe', 'HR', 50000.00),
+    ('Jane Smith', 'HR', 55000.00),
+    ('Michael Johnson', 'HR', 60000.00),
+    ('Emily Davis', 'IT', 60000.00),
+    ('David Brown', 'IT', 65000.00),
+    ('Sarah Wilson', 'Finance', 70000.00),
+    ('Robert Taylor', 'Finance', 75000.00),
+    ('Jennifer Martinez', 'Finance', 80000.00);
+
+/*
+-- Q.
+You have a table with below COLUMNS
+emp_id employee_name, department, salary
+
+
+Find the details of employees whose salary is greater
+than the average salary across the entire company.
+*/
+
+SELECT * FROM employees;
+select * from employees
+where salary > (select avg(salary) from employees);
