22203f0e8318663eedcd7fb12e492a485c2fe7c4
Sonalp98 <165366403+Sonalp98@users.noreply.github.com>
2024-05-17 14:04:41 +0530

    Add files via upload

---
+++ 49(Busiest Day).sql
@@ -0,0 +1,55 @@
+-- SQL Challenge 49/50
+
+DROP TABLE IF EXISTS orders;
+-- Create table for orders
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    order_date DATE
+);
+
+-- Insert sample records for orders
+INSERT INTO orders (order_date)
+VALUES
+    ('2024-05-01'),
+    ('2024-05-01'),
+    ('2024-05-01'),
+    ('2024-05-02'),
+    ('2024-05-02'),
+    ('2024-05-02'),
+    ('2024-05-03'),
+    ('2024-05-03'),
+    ('2024-05-03'),
+    ('2024-05-03'),
+    ('2024-05-03'),
+    ('2024-05-04'),
+    ('2024-05-04'),
+    ('2024-05-04'),
+    ('2024-05-04'),
+    ('2024-05-04'),
+    ('2024-05-05'),
+    ('2024-05-05'),
+    ('2024-05-05'),
+    ('2024-05-05'),
+    ('2024-05-06'),
+    ('2024-05-06'),
+    ('2024-05-06'),
+    ('2024-05-06'),
+    ('2024-05-06');
+
+-- Display the records to verify
+
+
+/*
+-- Question:
+You are given a orders table with
+columns order_id, order_date
+
+Identify the busiest day for orders along 
+with the total number of orders placed on that day. 
+*/
+
+SELECT * FROM orders;
+select order_date, count(*) as total_orders
+from orders
+group by order_date
+order by total_orders desc;
